9d3034d8518e597ea5b738201bafb1e41729405a
Siebe Vanden Eynden <siebe.vandeneynden@student.kdg.be>
2016-11-05 00:47:59 +0100

    changed cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-	    $schedule->command('check:competition')->everyHour();
+	    $schedule->command('check:competition')->hourly();
     }
 
     /**
